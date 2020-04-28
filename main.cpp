1749e60649ecb212fb87478951d7ff8835e2a54d
amq <amq@users.noreply.github.com>
2020-04-28 23:18:23 +0200

    Add .clang-format

--- main.cpp
+++ main.cpp
@@ -1,39 +1,36 @@
 #define CL_TARGET_OPENCL_VERSION 120
 #define __CL_ENABLE_EXCEPTIONS
-#define  _VARIADIC_MAX 10
+#define _VARIADIC_MAX 10
 #include <CL/cl.hpp>
 #include <iostream>
 #include <vector>
 
 const int numElements = 32;
 
-int main(void)
-{
-	cl::Program addProgram(
+int main(void) {
+    cl::Program addProgram(
         cl::STRING_CLASS(
-		"int add(int a, int b) { return a + b; }")
-		, false);
-	cl::Program vectorWrapper(
+            "int add(int a, int b) { return a + b; }"),
+        false);
+    cl::Program vectorWrapper(
         cl::STRING_CLASS(
-		"int add(int a, int b); kernel void vectorAdd(global const int *inputA, global const int *inputB, global int *output){output[get_global_id(0)] = add(inputA[get_global_id(0)], inputB[get_global_id(0)]);}")
-		, false);
-	std::vector<cl::Program> programs;
-	addProgram.compile();
-	vectorWrapper.compile();		
-	
+            "int add(int a, int b); kernel void vectorAdd(global const int *inputA, global const int *inputB, global int *output){output[get_global_id(0)] = add(inputA[get_global_id(0)], inputB[get_global_id(0)]);}"),
+        false);
+    std::vector<cl::Program> programs;
+    addProgram.compile();
+    vectorWrapper.compile();
+
     cl::STRING_CLASS s = addProgram.getInfo<CL_PROGRAM_SOURCE>();
-    
-	programs.push_back(addProgram);
-	programs.push_back(vectorWrapper);
-	cl::Program vectorAddProgram = cl::linkProgram(programs);
 
-    auto vectorAddKernel = 
+    programs.push_back(addProgram);
+    programs.push_back(vectorWrapper);
+    cl::Program vectorAddProgram = cl::linkProgram(programs);
+
+    auto vectorAddKernel =
         cl::make_kernel<
-            cl::Buffer&,
-            cl::Buffer&,
-            cl::Buffer&
-            >( vectorAddProgram, "vectorAdd" );
-
+            cl::Buffer &,
+            cl::Buffer &,
+            cl::Buffer &>(vectorAddProgram, "vectorAdd");
 
     std::vector<int> inputA(numElements, 1);
     std::vector<int> inputB(numElements, 2);
@@ -53,9 +50,8 @@ int main(void)
     cl::copy(outputBuffer, begin(output), end(output));
 
     std::cout << "Output:\n";
-    for( int i = 1; i < numElements; ++i ) {
+    for (int i = 1; i < numElements; ++i) {
         std::cout << "\t" << output[i] << "\n";
     }
     std::cout << "\n\n";
-
 }
